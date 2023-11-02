6434409c58377985988f05879026d9c3480e69d2
Shishira <shishiraiyar@gmail.com>
2023-11-02 23:59:38 +0530

    Added click

--- hardware/encoder_test/encoder_test.ino
+++ hardware/encoder_test/encoder_test.ino
@@ -5,11 +5,18 @@ PioEncoder encoder(2); // encoder is connected to GPIO2 and GPIO3
 void setup() {
   encoder.begin();
   Serial.begin(115200);
+  pinMode(4, INPUT_PULLUP);
   while(!Serial);
 
 }
 
 void loop() {
-  Serial.println(encoder.getCount());
-  delay(10);
-}
+  Serial.println(encoder.getCount()/2);
+  int mil = millis();
+  if(!digitalRead(4) && millis() - mil <200){
+    Serial.println("Click");
+    delay(500);
+  }
+
+  delay(100);
+} 
